f3b24bd6af43b775f092648bbac3b5d6379f949f
Ali Shaikh <shahinshaikh603@gmail.com>
2025-03-27 07:19:49 +0530

    Feat: Add about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import Hero from "./components/Hero.jsx";
+import About from "./components/about.jsx";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
-           <Hero/>
-
-            <section className='z-0 min-h-screen bg-blue-500'/>
+            <Hero/>
+            <About/>
         </main>
     )
 }
---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+    return (
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+            Disc<b>o</b>ver The World's <br/> L<b>a</b>rgest Shared Adventure
+        </div>
+    )
+}
+export default AnimatedTitle
